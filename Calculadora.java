d0d6bf16faabe42173ec198438e1bdced714987f
Juan Marcos Caicedo Mejia <1144110058@u.icesi.edu.co>
2025-02-14 12:17:48 -0500

    Agregue multiplicacion

--- Calculadora.java
+++ Calculadora.java
@@ -19,10 +19,12 @@ public class Calculadora {
         num2 = escaner.nextInt();
         // System.out.println("Numero 1 me dijiste: " + num1);
         // System.out.println("Numero 2 me dijiste: " + num2);
-        int suma, resta;
+        int suma, resta, producto;
         suma = num1 + num2;
         resta = num1 - num2;
+        producto = num1 * num2;
         System.out.println("La suma de los dos numeros es: " + suma);
         System.out.println("La resta entre numero 1 y numero 2 es: " + resta);
+        System.out.println("La multiplicacion entre numero 1 y numero 2 es: " + producto);
     }
 }
